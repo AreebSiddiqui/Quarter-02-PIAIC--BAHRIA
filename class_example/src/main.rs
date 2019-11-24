a7f0d98483eaf5a4513d0c60d7667239bf22aff7
AreebSiddiqui <areeb40@hotmail.com>
2019-11-24 15:44:06 +0500

    Updated 24/2019

--- class_example/src/main.rs
+++ class_example/src/main.rs
@@ -3,14 +3,17 @@ struct Spiderman {
     name: String,
 } //Type
 
+#[derive(Debug)]
 struct Superman {
     name: String,
 } //Type
 
+#[derive(Debug)]
 struct Batman {
     name: String,
 } //Type
 
+#[derive(Debug)]
 struct Hulk {
     name: String,
 } //Type
@@ -56,24 +59,24 @@ impl Power for Hulk {
 fn main () {
     let my_spiderman = Spiderman {
         name: String::from("MYSPIDERMAN"),
-    };
+    }; //my_spiderman is the instance Spiderman
 
     let my_superman = Superman {
         name: String::from("MYSUPERMAN"),
-    };
+    };//my_superman is the instance Superman
     
     let my_batman = Batman {
         name: String::from("MYBATMAN"),
-    };
+    };//my_batman is the instance Batman
     
     let my_hulk = Hulk {
         name: String::from("HULK"),
-    };
+    };//my_hulk is the instance Hulk
     
-    println!("{}",my_spiderman.power_score());
-    println!("{}",my_superman.power_score());
-    println!("{}",my_batman.power_score());
-    println!("{}",my_hulk.power_score());
+    println!("{}",my_spiderman.power_score());  //calling method
+    println!("{}",my_superman.power_score());  //calling method
+    println!("{}",my_batman.power_score());   //calling method
+    println!("{}",my_hulk.power_score());    //calling method
 
 }
 
